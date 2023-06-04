af430dd9ed8fab93b951563c4f4df9d5f67f70f2
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-06-04 15:48:58 +0500

    .

---
+++ scripts/devpopup.js
@@ -0,0 +1,10 @@
+const openPop = (id) => {
+    let popup = document.getElementById(id).style.display = "flex";
+    return popup;
+}
+
+const closePop = (id) => {
+    let popup = document.getElementById(id).style.display = "none";
+    return popup;
+}
+
